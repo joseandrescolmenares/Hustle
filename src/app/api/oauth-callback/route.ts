66c8840fd82a9c41abf165570249fcf5ae1b553c
jose <joseandrescolmenares02@gmail.com>
2023-10-20 14:23:08 -0300

    fix: test3

--- src/app/api/oauth-callback/route.ts
+++ src/app/api/oauth-callback/route.ts
@@ -30,7 +30,7 @@ export async function GET(request: Request) {
       );
 
       const { access_token, refresh_token } = response.data;
-
+       console.log(access_token, refresh_token)
       //   Aquí puedes guardar los tokens en la base de datos o en una cookie.
 
       //  redirect("http://localhost:3000/dashboard")
