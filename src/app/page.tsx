5e5b935c51fdab592f69c76cfacf95cb66da0341
jose <joseandrescolmenares02@gmail.com>
2023-10-20 16:56:41 -0300

    fix: test free

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
  
-    const hubspotAuthUrl = `https://app.hubspot.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_HUBSPOT_CLIENT_ID}&scope=${process.env.NEXT_PUBLIC_HUBSPOT_SCOPE}&redirect_uri=${process.env.NEXT_PUBLIC_HUBSPOT_REDIRECT_URI}`;
+    const hubspotAuthUrl = `https://app.hubspot.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_HUBSPOT_CLIENT_ID}&scope=${'crm.objects.contacts.read'}&redirect_uri=${process.env.NEXT_PUBLIC_HUBSPOT_REDIRECT_URI}`;
     redirect(hubspotAuthUrl);
 }
 
