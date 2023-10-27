402581c47a034c461240c5af8b07c08a65378950
jose <joseandrescolmenares02@gmail.com>
2023-10-27 01:33:55 -0300

    fix: validate environment variable

--- src/lib/Auth.ts
+++ src/lib/Auth.ts
@@ -1,8 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseKey || !supabaseUrl) {
+  throw new Error("La clave de Supabase no está definida.");
+}
+
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  supabaseUrl,
+  supabaseKey
 );
 
 const handleAuthSignup = async () => {
