0097fdec2f91d1f0b50d4595b6b0596941012b20
jose <joseandrescolmenares02@gmail.com>
2023-11-08 11:45:41 -0300

    fix:  test deploy route slack

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -10,43 +10,46 @@ export async function GET(request: Request) {
   const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
-  try {
-    const responseToken: any = await axios.post(
-      "https://slack.com/api/oauth.v2.access",
-      null,
-      {
-        params: {
-          grant_type: "authorization_code",
-          client_id: process.env.SLACK_CLIENT_ID,
-          client_secret: process.env.SLACK_CLIENT_SECRET,
-          code: code,
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-    const result = responseToken.data;
-    const { data, error } = await supabase
-      .from("integrations")
-      .update({
-        isSlack: true,
-        webhookUrlSlack: result?.incoming_webhook?.url,
-        accessTokenSlack: result?.access_token,
-        channelSlack: result?.incoming_webhook?.channel,
-        teamSlack: result?.team,
-      })
-      .eq("userId", userId)
-      .select();
+  if (code) {
+    try {
+      const responseToken: any = await axios.post(
+        "https://slack.com/api/oauth.v2.access",
+        null,
+        {
+          params: {
+            grant_type: "authorization_code",
+            client_id: process.env.SLACK_CLIENT_ID,
+            client_secret: process.env.SLACK_CLIENT_SECRET,
+            code: code,
+          },
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      const result = responseToken?.data;
+      console.log(result, "result");
+      const { data, error } = await supabase
+        .from("integrations")
+        .update({
+          isSlack: true,
+          webhookUrlSlack: result?.incoming_webhook?.url,
+          accessTokenSlack: result?.access_token,
+          channelSlack: result?.incoming_webhook?.channel,
+          teamSlack: result?.team,
+        })
+        .eq("userId", userId)
+        .select();
 
-      console.log(data, "dataSlack")
+      console.log(data, "dataSlack");
 
-    if (error) return NextResponse.json({ error: "error" });
-  } catch (error) {
-    console.log(error);
-    return Response.json({
-      error: "Hubo un error al obtener el token de acceso.",
-    });
+      if (error) return NextResponse.json({ error: "error" });
+    } catch (error) {
+      console.log(error);
+      return Response.json({
+        error: "Hubo un error al obtener el token de acceso.",
+      });
+    }
   }
 
   // redirect("/dashboard");
