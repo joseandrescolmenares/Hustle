10e0b366c8530964d5ffd828176f8f456a07485f
jose <joseandrescolmenares02@gmail.com>
2023-11-23 01:38:39 -0300

    fix: add route onboarding

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,12 @@ export const config = {
   matcher: "/dashboard",
 };
 export function middleware(request: NextRequest) {
+
   if (!request.cookies.get("access_token") ) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+  // else if(!request.cookies.get("role")){
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
   return NextResponse.next();
 }
