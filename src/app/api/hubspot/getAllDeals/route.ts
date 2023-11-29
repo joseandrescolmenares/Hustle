32f098a347a40701ba757df343a61555131a5ca0
jose <joseandrescolmenares02@gmail.com>
2023-11-29 16:12:59 -0300

    fix: add time

--- src/app/api/hubspot/getAllDeals/route.ts
+++ src/app/api/hubspot/getAllDeals/route.ts
@@ -62,7 +62,7 @@ async function fetchAllDeals(): Promise<any> {
         break;
       }
 
-      await sleep(500); // Simulación de trabajo
+      await sleep(5000); // Simulación de trabajo
     }
 
     return allData;
