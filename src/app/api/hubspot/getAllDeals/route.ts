db48c80cd33bc2f78d0b2062634089ac0bc72a37
jose <joseandrescolmenares02@gmail.com>
2023-11-29 22:30:19 -0300

    fix: change request alldeasl

--- src/app/api/hubspot/getAllDeals/route.ts
+++ src/app/api/hubspot/getAllDeals/route.ts
@@ -351,83 +351,7 @@ async function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// async function fetchAllDeals(): Promise<any> {
-//   try {
-//     if (isExecuting) {
-//       console.log("Otra instancia en curso, abortando...");
-//       return;
-//     }
 
-//     isExecuting = true;
-
-//     if (lock) {
-//       console.log("Esperando a que se libere el bloqueo...");
-//       while (lock) {
-//         await sleep(1000);
-//       }
-//     }
-
-//     lock = true;
-
-//     let allData: any[] = [];
-//     let url =
-//       "https://api.hubapi.com/crm/v3/objects/deals?properties=hubspot_owner_id,dealname,dealstage,amount,num_associated_contacts,closedate,hs_priority,notes_last_contacted,hs_all_collaborator_owner_ids,hs_is_closed_won,description,hubspot_owner_assigneddate,notes_last_updated,closed_won_reason,closed_lost_reason,num_contacted_notes,hs_next_step,hs_forecast_probability,hs_deal_stage_probability&associations=notes&limit=20";
-
-//     let requestCount = 0;
-//     const maxRequestsPerInterval = 100;
-//     const intervalDuration = 10 * 1000;
-
-//     while (url) {
-//       const resultDeals = await getAllDeals(url);
-//       const results = resultDeals.results;
-
-//       for (const deal of results) {
-//         const ownerInfo = await getIOwner(
-//           deal.properties.hubspot_owner_id || "",
-//           deal.properties.dealname || "",
-//           deal.properties.hs_object_id || "",
-//           deal.properties.num_associated_contacts,
-//           deal.properties.amount || "",
-//           deal.properties.closed_lost_reason || "",
-//           deal.properties.closed_won_reason || "",
-//           deal.properties.closedate || "",
-//           deal.properties.createdate || "",
-//           deal.properties.dealstage || "",
-//           deal.properties.description || "",
-//           deal.properties.hs_all_collaborator_owner_ids || "",
-//           deal.properties.hs_deal_stage_probability || "",
-//           deal.properties.hs_forecast_probability || "",
-//           deal.properties.hs_is_closed_won || "",
-//           deal.properties.hs_lastmodifieddate || "",
-//           deal.properties.hs_next_step || "",
-//           deal.properties.hs_priority || "",
-//           deal.properties.num_contacted_notes
-//         );
-//         allData.push(ownerInfo);
-//       }
-
-//       url = resultDeals?.paging?.next?.link || "";
-
-//       requestCount++;
-//       if (requestCount >= maxRequestsPerInterval) {
-//         await sleep(intervalDuration);
-//         requestCount = 0;
-//       }
-
-//       if (!url) {
-//         break;
-//       }
-//     }
-
-//     return allData;
-//   } catch (error) {
-//     console.error("Error:", error);
-//     throw new Error("Hubo un error al obtener las negociaciones");
-//   } finally {
-//     isExecuting = false;
-//     lock = false;
-//   }
-// }
 async function fetchAllDeals(): Promise<any[]> {
   try {
     if (isExecuting) {
@@ -515,7 +439,7 @@ export async function GET(request: Request) {
   try {
     const result = await fetchAllDeals();
 
-    if (result.length > 0) {
+    if (result) {
       const { data: dataAllDeals, error } = await supabase
         .from("deals")
         .insert(result)
