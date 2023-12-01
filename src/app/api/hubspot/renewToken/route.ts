d0f67685b8e015e3f6c8dae2f12ecf357116c28b
jose <joseandrescolmenares02@gmail.com>
2023-12-01 12:06:23 -0300

    fix

--- src/app/api/hubspot/renewToken/route.ts
+++ src/app/api/hubspot/renewToken/route.ts
@@ -1,43 +1,43 @@
-// import axios from "axios";
+import axios from "axios";
 
-// import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 
-// import { redirect } from "next/navigation";
-// import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
-// export async function GET(request : Request){
+export async function GET(request : Request){
 
-//   const clientId = process.env.HUBSPOT_CLIENT_ID;
-//   const clientSecret = process.env.HUBSPOT_CLIENT_SECRET;
-//   const redirectUri = process.env.HUBSPOT_REDIRECT_URI;
+  const clientId = process.env.HUBSPOT_CLIENT_ID;
+  const clientSecret = process.env.HUBSPOT_CLIENT_SECRET;
+  const redirectUri = process.env.HUBSPOT_REDIRECT_URI;
 
-//   const cookieStore = cookies();
-//   const refresh_token = cookieStore.get("refresh_token")?.value;
+  const cookieStore = cookies();
+  const refresh_token = cookieStore.get("refresh_token")?.value;
 
-//   try {
-//     const response = await axios.post(
-//       "https://api.hubapi.com/oauth/v1/token",
-//       null,
-//       {
-//         params: {
-//           grant_type: "refresh_token",
-//           client_id: clientId,
-//           client_secret: clientSecret,
-//           redirect_uri: redirectUri,
-//           refresh_token: refresh_token,
-//         },
-//       }
-//     );
+  try {
+    const response = await axios.post(
+      "https://api.hubapi.com/oauth/v1/token",
+      null,
+      {
+        params: {
+          grant_type: "refresh_token",
+          client_id: clientId,
+          client_secret: clientSecret,
+          redirect_uri: redirectUri,
+          refresh_token: refresh_token,
+        },
+      }
+    );
 
-//     if (response.data.access_token) {
-//       cookieStore.set("accessTokenHubspot", response.data.access_token);
-//       cookieStore.set("refresh_token", response.data.refresh_token);
-//       cookieStore.set("expires_in", response.data.expires_in);
-//     }
+    if (response.data.access_token) {
+      cookieStore.set("accessTokenHubspot", response.data.access_token)
+      cookieStore.set("refresh_token", response.data.refresh_token);
+      cookieStore.set("expires_in", response.data.expires_in);
+    }
   
 
-//   } catch (error) {
-//     console.error("Error al renovar el token de acceso:", error);
-//   }
-//   redirect("/dashboard")
-// }
+  } catch (error) {
+    console.error("Error al renovar el token de acceso:", error);
+  }
+  redirect("/dashboard")
+}
