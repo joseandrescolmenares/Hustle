b39e6b10a09c9e2b693182df663dbedaacb9d783
jose <joseandrescolmenares02@gmail.com>
2023-12-01 12:18:54 -0300

    fix : change supaase

--- src/lib/ClientSupabase.ts
+++ src/lib/ClientSupabase.ts
@@ -1,14 +1,13 @@
 
 import { AuthResponse, createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://vgiqwxcbhyuspfkxvwfe.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnaXF3eGNiaHl1c3Bma3h2d2ZlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5Nzg2NDY0NywiZXhwIjoyMDEzNDQwNjQ3fQ.-89CIVc7b-sxSvfebSzOF3UOZiLx8xXVu8Y7sxJfzt0";
 
 if (!supabaseKey || !supabaseUrl) {
   throw new Error("La clave de Supabase no está definida.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
 
 
