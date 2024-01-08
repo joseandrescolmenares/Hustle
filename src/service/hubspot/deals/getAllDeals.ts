83f8d7a2de82a4153391aec7c8c4541e5be99527
jose <joseandrescolmenares02@gmail.com>
2024-01-08 11:53:48 -0300

    fix: replace axios

--- src/service/hubspot/deals/getAllDeals.ts
+++ src/service/hubspot/deals/getAllDeals.ts
@@ -1,6 +1,29 @@
-import axios from "axios";
+// import axios from "axios";
 import { Console } from "console";
 import { cookies } from "next/headers";
+
+// export const getAllDeals = async (url: string) => {
+//   const cookiesStore = cookies();
+//   const cookieToken = cookiesStore.get("accessTokenHubspot")?.value;
+//   try {
+//     const headers = {
+//       Authorization: `Bearer ${cookieToken}`,
+//       "Content-Type": "application/json",
+//     };
+  
+  
+//     const urlDeals = url;
+
+//     const responseData: any = await axios.get(urlDeals, { headers });
+//     const dataCompanies = responseData?.data;
+
+//     return dataCompanies;
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error();
+//   }
+// };
+
 
 export const getAllDeals = async (url: string) => {
   const cookiesStore = cookies();
@@ -10,12 +33,17 @@ export const getAllDeals = async (url: string) => {
       Authorization: `Bearer ${cookieToken}`,
       "Content-Type": "application/json",
     };
-  
-  
+
     const urlDeals = url;
 
-    const responseData: any = await axios.get(urlDeals, { headers });
-    const dataCompanies = responseData?.data;
+    const response = await fetch(urlDeals, { headers });
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    const dataCompanies = responseData;
 
     return dataCompanies;
   } catch (error) {
