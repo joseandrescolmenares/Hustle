2ed9feaa9363c12df9a55c6884b7e459662bb571
jose <joseandrescolmenares02@gmail.com>
2024-01-19 15:35:59 -0300

    fix: change url associe

--- src/app/api/agentAi/route.ts
+++ src/app/api/agentAi/route.ts
@@ -1,182 +1,182 @@
-import { ChatOpenAI } from "@langchain/openai";
-import type { ChatPromptTemplate } from "@langchain/core/prompts";
-import { createOpenAIToolsAgent, AgentExecutor } from "langchain/agents";
-import { pull } from "langchain/hub";
-import { renewToken } from "@/service/renewToken";
-import { supabase } from "@/lib/ClientSupabase";
-import { ZodObject, string, z } from "zod";
-import {
-  DynamicTool,
-  DynamicStructuredTool,
-} from "@langchain/community/tools/dynamic";
-import { NextResponse } from "next/server";
+// import { ChatOpenAI } from "@langchain/openai";
+// import type { ChatPromptTemplate } from "@langchain/core/prompts";
+// import { createOpenAIToolsAgent, AgentExecutor } from "langchain/agents";
+// import { pull } from "langchain/hub";
+// import { renewToken } from "@/service/renewToken";
+// import { supabase } from "@/lib/ClientSupabase";
+// import { ZodObject, string, z } from "zod";
+// import {
+//   DynamicTool,
+//   DynamicStructuredTool,
+// } from "@langchain/community/tools/dynamic";
+// import { NextResponse } from "next/server";
 
-// export const runtime = "edge";
+// // export const runtime = "edge";
 
-export async function GET(request: Request) {
-  // const requestBody = await request.json();
+// export async function GET(request: Request) {
+//   // const requestBody = await request.json();
 
-  const { data, error } = await supabase
-    .from("teams")
-    .select(
-      `id_integrations (
-      refresh_token
-      )`
-    )
-    .eq("hubspotAccount", 44497831);
-  if (data == null) return;
+//   const { data, error } = await supabase
+//     .from("teams")
+//     .select(
+//       `id_integrations (
+//       refresh_token
+//       )`
+//     )
+//     .eq("hubspotAccount", 24167306);
+//   if (data == null) return;
 
-  const { refresh_token }: any = data[0]?.id_integrations;
+//   const { refresh_token }: any = data[0]?.id_integrations;
 
-  if(!refresh_token) return
+//   if(!refresh_token) return
 
-  const token = await renewToken(refresh_token);
+//   const token = await renewToken(refresh_token);
 
-  console.log(token, "data");
+//   console.log(token, "data");
 
-  const tools = [
-    new DynamicStructuredTool({
-      name: "createDeals",
-      description:
-        "This function creates a deal with the given properties. The function ensures that all information is accurately recorded to enable effective management and tracking of the newly created deal.",
-      schema: z.object({
-        amount: z
-          .number()
-          .describe(
-            "represents the monetary amount associated with the deal being created."
-          )
-          .optional()
-          .default(0),
-        dealname: z
-          .string()
-          .describe("represents the name of the deal.")
-          .optional()
-          .default(""),
-        dealstage: z
-          .string()
-          .describe(
-            "The stage of the deal. The business stages allow you to classify and monitor the progress of the businesses you are working on."
-          )
-          .optional()
-          .default(""),
-        closedate: z
-          .string()
-          .describe("The date the deal was closed")
-          .optional(),
-      }),
-      func: async ({
-        amount,
-        dealname,
-        dealstage,
-        closedate,
-      }): Promise<any> => {
-        const url = "https://api.hubapi.com/crm/v3/objects/deals";
+//   const tools = [
+//     new DynamicStructuredTool({
+//       name: "createDeals",
+//       description:
+//         "This function creates a deal with the given properties. The function ensures that all information is accurately recorded to enable effective management and tracking of the newly created deal.",
+//       schema: z.object({
+//         amount: z
+//           .number()
+//           .describe(
+//             "represents the monetary amount associated with the deal being created."
+//           )
+//           .optional()
+//           .default(0),
+//         dealname: z
+//           .string()
+//           .describe("represents the name of the deal.")
+//           .optional()
+//           .default(""),
+//         dealstage: z
+//           .string()
+//           .describe(
+//             "The stage of the deal. The business stages allow you to classify and monitor the progress of the businesses you are working on."
+//           )
+//           .optional()
+//           .default(""),
+//         closedate: z
+//           .string()
+//           .describe("The date the deal was closed")
+//           .optional(),
+//       }),
+//       func: async ({
+//         amount,
+//         dealname,
+//         dealstage,
+//         closedate,
+//       }): Promise<any> => {
+//         const url = "https://api.hubapi.com/crm/v3/objects/deals";
 
-        const requestBody = {
-          properties: {
-            amount,
-            dealstage,
-            dealname,
-            closedate,
-          },
-        };
-        try {
-          const response = await fetch(url, {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(requestBody),
-          });
-          const data = await response.json();
-          console.log(data);
-          return `se ha creado con exito, puedes ver mas detalle https://app.hubspot.com/contacts/44497831/record/0-3/${data.id}`;
-        } catch (error: any) {
-          if (error.response.data.category == "EXPIRED_AUTHENTICATION") {
-            return "token expired";
-          }
-          return "error, por favor vuelva a intentarlo";
-        }
-      },
-    }),
+//         const requestBody = {
+//           properties: {
+//             amount,
+//             dealstage,
+//             dealname,
+//             closedate,
+//           },
+//         };
+//         try {
+//           const response = await fetch(url, {
+//             method: "POST",
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(requestBody),
+//           });
+//           const data = await response.json();
+//           console.log(data);
+//           return `se ha creado con exito, puedes ver mas detalle https://app.hubspot.com/contacts/44497831/record/0-3/${data.id}`;
+//         } catch (error: any) {
+//           if (error.response.data.category == "EXPIRED_AUTHENTICATION") {
+//             return "token expired";
+//           }
+//           return "error, por favor vuelva a intentarlo";
+//         }
+//       },
+//     }),
 
-    new DynamicStructuredTool({
-      name: "updateDealData",
-      description: "This function is responsible for updating the crm data.",
-      schema: z.object({
-        amount: z
-          .number()
-          .describe(
-            "represents the monetary amount associated with the deal being created."
-          )
-          .optional(),
-      }),
-      func: async ({ amount }) => `este es se actulizo `,
-    }),
+//     new DynamicStructuredTool({
+//       name: "updateDealData",
+//       description: "This function is responsible for updating the crm data.",
+//       schema: z.object({
+//         amount: z
+//           .number()
+//           .describe(
+//             "represents the monetary amount associated with the deal being created."
+//           )
+//           .optional(),
+//       }),
+//       func: async ({ amount }) => `este es se actulizo `,
+//     }),
 
-    new DynamicStructuredTool({
-      name: "createDealBusinessAssociation",
-      description:
-        "this function creates associations between deal and business.",
-      schema: z.object({
-        amount: z
-          .number()
-          .describe(
-            "represents the monetary amount associated with the deal being created."
-          )
-          .optional(),
-      }),
-      func: async ({ amount }) => {
-        const url = `https://api.hubapi.com/crm/v4/objects/deal/16213921559/associations/default/company/18209854732`;
+//     new DynamicStructuredTool({
+//       name: "createDealBusinessAssociation",
+//       description:
+//         "this function creates associations between deal and business.",
+//       schema: z.object({
+//         amount: z
+//           .number()
+//           .describe(
+//             "represents the monetary amount associated with the deal being created."
+//           )
+//           .optional(),
+//       }),
+//       func: async ({ amount }) => {
+//         const url = `https://api.hubapi.com/crm/v4/objects/deal/16213921559/associations/default/company/18209854732`;
 
-        const requestBody = {
-          properties: {
-            amount,
-            // dealstage,
-            // dealname,
-            // closedate,
-          },
-        };
+//         const requestBody = {
+//           properties: {
+//             amount,
+//             // dealstage,
+//             // dealname,
+//             // closedate,
+//           },
+//         };
 
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestBody),
-        });
+//         const response = await fetch(url, {
+//           method: "POST",
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(requestBody),
+//         });
 
-        return "";
-      },
-    }),
-  ];
+//         return "";
+//       },
+//     }),
+//   ];
 
-  const llm = new ChatOpenAI({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    modelName: "gpt-3.5-turbo-1106",
-    temperature: 0,
-  });
-  const prompt = await pull<ChatPromptTemplate>("jose/openai-tools-agent");
+//   const llm = new ChatOpenAI({
+//     openAIApiKey: process.env.OPENAI_API_KEY,
+//     modelName: "gpt-3.5-turbo-1106",
+//     temperature: 0,
+//   });
+//   const prompt = await pull<ChatPromptTemplate>("jose/openai-tools-agent");
 
-  const agent = await createOpenAIToolsAgent({
-    llm,
-    tools,
-    prompt,
-  });
+//   const agent = await createOpenAIToolsAgent({
+//     llm,
+//     tools,
+//     prompt,
+//   });
 
-  const agentExecutor = new AgentExecutor({
-    agent,
-    tools,
-  });
+//   const agentExecutor = new AgentExecutor({
+//     agent,
+//     tools,
+//   });
 
-  const result = await agentExecutor.invoke({
-    input: "crea un negocio que se llame josecito y que de monto de 10000$",
-    chat_history: [],
-  });
+//   const result = await agentExecutor.invoke({
+//     input: "crea un negocio que se llame josecito y que de monto de 10000$",
+//     chat_history: [],
+//   });
 
-  console.log(result);
+//   console.log(result);
 
-  return NextResponse.json({ sucess: result });
-}
+//   return NextResponse.json({ sucess: result });
+// }
--- src/app/api/agentAi/testAgent/route.ts
+++ src/app/api/agentAi/testAgent/route.ts
@@ -1,52 +1,52 @@
-import { supabase } from "@/lib/ClientSupabase";
-import { NextResponse } from "next/server";
-import { renewToken } from "@/service/renewToken";
+// import { supabase } from "@/lib/ClientSupabase";
+// import { NextResponse } from "next/server";
+// import { renewToken } from "@/service/renewToken";
 
-import axios from "axios";
+// import axios from "axios";
 
-export async function GET(request: Request) {
-  const { data, error } = await supabase
-    .from("teams")
-    .select(
-      `id_integrations (
-      refresh_token
-      )`
-    )
-    .eq("hubspotAccount", 44497831);
-  if (data == null) return;
+// export async function GET(request: Request) {
+//   const { data, error } = await supabase
+//     .from("teams")
+//     .select(
+//       `id_integrations (
+//       refresh_token
+//       )`
+//     )
+//     .eq("hubspotAccount", 44497831);
+//   if (data == null) return;
 
-  const { refresh_token }: any = data[0]?.id_integrations;
-  const token = await renewToken(refresh_token);
-  console.log(token, "data");
+//   const { refresh_token }: any = data[0]?.id_integrations;
+//   const token = await renewToken(refresh_token);
+//   console.log(token, "data");
 
-  const url = `https://api.hubapi.com/crm/v4/objects/deal/16213921559/associations/default/company/18209854732`; // Replace "dealId" with the actual ID of the deal you want to update  //   properties: {
+//   const url = `https://api.hubapi.com/crm/v4/objects/deal/16213921559/associations/default/company/18209854732`; // Replace "dealId" with the actual ID of the deal you want to update  //   properties: {
 
-  // const requestBody = {
-  //     amount: 0,
-  //     dealname: "jose test",
-  //     // dealstage: "",
-  //     closedate: "",
-  //   },
-  // };
+//   // const requestBody = {
+//   //     amount: 0,
+//   //     dealname: "jose test",
+//   //     // dealstage: "",
+//   //     closedate: "",
+//   //   },
+//   // };
 
-  try {
-    const response = await axios.put(
-      url,
-      // requestBody,
+//   try {
+//     const response = await axios.put(
+//       url,
+//       // requestBody,
 
-      {
-        headers: {
-          Authorization: `Bearer `,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+//       {
+//         headers: {
+//           Authorization: `Bearer `,
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
 
-    const data = response.data;
-    console.log(data);
-  return NextResponse.json({ ok: token });
-  } catch (error) {
-    console.error("Error updating deal:", error);
-    return NextResponse.json({ ok: false });
-  }
-}
+//     const data = response.data;
+//     console.log(data);
+//   return NextResponse.json({ ok: token });
+//   } catch (error) {
+//     console.error("Error updating deal:", error);
+//     return NextResponse.json({ ok: false });
+//   }
+// }
