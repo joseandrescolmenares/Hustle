ed9fed162ce11514616e9d78fad34f0501f9ed2e
jose <joseandrescolmenares02@gmail.com>
2024-01-31 00:36:16 -0300

    fix: require variable enviroment

--- src/lib/ClientSupabase.ts
+++ src/lib/ClientSupabase.ts
@@ -1,7 +1,6 @@
 
 import { AuthResponse, createClient } from "@supabase/supabase-js";
-import * as dotenv from 'dotenv';
-dotenv.config();
+require("dotenv").config()
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
