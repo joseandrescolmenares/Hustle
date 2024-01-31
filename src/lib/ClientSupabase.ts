be299752823078bd03ec44b8eb015040d4030dda
jose <joseandrescolmenares02@gmail.com>
2024-01-31 00:43:07 -0300

    fix: change variable enviroment supabase

--- src/lib/ClientSupabase.ts
+++ src/lib/ClientSupabase.ts
@@ -1,9 +1,12 @@
 
 import { AuthResponse, createClient } from "@supabase/supabase-js";
-require("dotenv").config()
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+console.log("VARIABLE, ", process.env)
+console.log("VARIABLE DUPABASE , ", process.env.NEXT_PUBLIC_SUPABASE_URL)
+console.log("VARIABLE  ANO, ", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
 if (!supabaseKey || !supabaseUrl) {
   throw new Error("La clave de Supabase no está definida.");
