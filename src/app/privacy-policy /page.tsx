8ec73f1844c95b16f3f4e13dc4d5d3649721627f
jose <joseandrescolmenares02@gmail.com>
2024-02-20 23:16:08 -0300

    fix: add page pravy-policy

--- src/app/privacy-policy /page.tsx
+++ src/app/privacy-policy /page.tsx
@@ -1,94 +1,96 @@
 "use client";
 import React from "react";
 
-export const page = () => {
-  return <div>Política de Privacidad de Meethustle.io
-
-  Última actualización: 21/02/20124
-  
-  Meethustle.io (`nosotros``, `nuestro`` o `nuestros``) opera el sitio web Meethustle.io (el `Servicio``).
-  
-  Esta página le informa sobre nuestras políticas con respecto a la recopilación, uso y divulgación de información personal cuando utiliza nuestro Servicio.
-  
-  No utilizaremos ni compartiremos su información con nadie, excepto como se describe en esta Política de Privacidad.
-  
-  Utilizamos su información personal para proporcionar y mejorar el Servicio. Al utilizar el Servicio, usted acepta la recopilación y el uso de información de acuerdo con esta política. A menos que se defina lo contrario en esta Política de Privacidad, los términos utilizados en esta Política de Privacidad tienen los mismos significados que en nuestros Términos y Condiciones, accesibles desde Meethustle.io.
-  
-  Recopilación y uso de información
-  
-  Recopilamos varios tipos diferentes de información con diversas finalidades para proporcionar y mejorar nuestro Servicio.
-  
-  Tipos de datos recopilados
-  
-  Datos personales
-  Mientras utiliza nuestro Servicio, es posible que le solicitemos que nos proporcione cierta información personalmente identificable que puede incluir, entre otros, su nombre, dirección de correo electrónico y número de teléfono. Los datos personales (`Datos``) pueden ser utilizados para contactarlo o identificarlo.
-  
-  Datos de uso
-  También podemos recopilar información sobre cómo se accede y utiliza el Servicio (`Datos de uso``). Estos Datos de uso pueden incluir información como la dirección del protocolo de Internet de su computadora (por ejemplo, dirección IP), tipo de navegador, versión del navegador, las páginas de nuestro Servicio que visita, la hora y fecha de su visita, el tiempo dedicado a esas páginas, identificadores de dispositivos y otros datos de diagnóstico.
-  
-  Uso de datos
-  
-  Meethustle.io utiliza los datos recopilados para diversos fines:
-  
-  Para proporcionar y mantener el Servicio
-  Para notificarle sobre cambios en nuestro Servicio
-  Para permitirle participar en funciones interactivas de nuestro Servicio cuando decida hacerlo
-  Para brindar asistencia al cliente
-  Para recopilar análisis o información valiosa que nos permitan mejorar nuestro Servicio
-  Para controlar el uso de nuestro Servicio
-  Para detectar, prevenir y abordar problemas técnicos
-  Transferencia de datos
-  
-  Su información, incluidos los Datos personales, puede ser transferida a —y mantenida en— computadoras ubicadas fuera de su estado, provincia, país u otra jurisdicción gubernamental donde las leyes de protección de datos pueden diferir de las de su jurisdicción.
-  
-  Si usted se encuentra fuera de Argentina y elige proporcionarnos información, tenga en cuenta que transferimos los datos, incluidos los Datos personales, a Argentina y que los procesamos allí.
-  
-  Su consentimiento a esta Política de Privacidad seguido de su envío de dicha información representa su acuerdo con dicha transferencia.
-  
-  Meethustle.io tomará todas las medidas razonablemente necesarias para garantizar que sus datos sean tratados de forma segura y de conformidad con esta Política de Privacidad y no se realizará ninguna transferencia de sus Datos personales a una organización o país a menos que existan controles adecuados, incluida la seguridad de sus datos y otra información personal.
-  
-  Divulgación de datos
-  
-  Requisitos legales
-  
-  Meethustle.io puede divulgar sus Datos personales de buena fe cuando considere que esta acción es necesaria para:
-  
-  Cumplir con una obligación legal
-  Proteger y defender los derechos o la propiedad de Meethustle.io
-  Prevenir o investigar posibles irregularidades en relación con el Servicio
-  Proteger la seguridad personal de los usuarios del Servicio o del público
-  Protegerse contra la responsabilidad legal
-  Seguridad de datos
-  
-  La seguridad de sus datos es importante para nosotros, pero recuerde que ningún método de transmisión por Internet o método de almacenamiento electrónico es 100% seguro. Si bien nos esforzamos por utilizar medios comercialmente aceptables para proteger sus Datos personales, no podemos garantizar su seguridad absoluta.
-  
-  Proveedores de servicios
-  
-  Podemos emplear a empresas e individuos de terceros para facilitar nuestro Servicio (`Proveedores de servicios``), para proporcionar el Servicio en nuestro nombre, para realizar servicios relacionados con el Servicio o para ayudarnos a analizar cómo se utiliza nuestro Servicio.
-  
-  Estos terceros tienen acceso a sus Datos personales solo para realizar estas tareas en nuestro nombre y están obligados a no divulgarlos ni utilizarlos para ningún otro propósito.
-  
-  Enlaces a otros sitios
-  
-  Nuestro Servicio puede contener enlaces a otros sitios que no son operados por nosotros. Si hace clic en un enlace de un tercero, será dirigido al sitio de ese tercero. Le recomendamos encarecidamente que revise la Política de privacidad de cada sitio que visite.
-  
-  No tenemos control ni asumimos responsabilidad alguna por el contenido, las políticas de privacidad o las prácticas de sitios o servicios de terceros.
-  
-  Privacidad de los menores
-  
-  Nuestro Servicio no está dirigido a personas menores de 18 años (`Menores`).
-  
-  No recopilamos a sabiendas información personalmente identificable de menores de 18 años. Si usted es padre/madre o tutor y sabe que su hijo nos ha proporcionado Datos personales, comuníquese con nosotros. Si nos enteramos de que hemos recopilado Datos personales de menores sin verificación del consentimiento parental, tomamos medidas para eliminar esa información de nuestros servidores.
-  
-  Cambios en esta Política de Privacidad
-  
-  Podemos actualizar nuestra Política de privacidad de vez en cuando. Le notificaremos cualquier cambio publicando la nueva Política de privacidad en esta página.
-  
-  Le recomendamos que revise esta Política de privacidad periódicamente para ver si hay cambios. Los cambios a esta Política de privacidad son efectivos cuando se publican en esta página.
-  
-  Contacte con nosotros
-  
-  Si tiene alguna pregunta sobre esta Política de privacidad, puede ponerse en contacto con nosotros:
-  
-  Por correo electrónico: joseandrescolmenares02@gmail.com</div>;
+const Privacy = () => {
+  return (
+    <div>
+      Política de Privacidad de Meethustle.io Última actualización: 21/02/20124
+      Meethustle.io (`nosotros``, `nuestro`` o `nuestros``) opera el sitio web
+      Meethustle.io (el `Servicio``). Esta página le informa sobre nuestras
+      políticas con respecto a la recopilación, uso y divulgación de información
+      personal cuando utiliza nuestro Servicio. No utilizaremos ni compartiremos
+      su información con nadie, excepto como se describe en esta Política de
+      Privacidad. Utilizamos su información personal para proporcionar y mejorar
+      el Servicio. Al utilizar el Servicio, usted acepta la recopilación y el
+      uso de información de acuerdo con esta política. A menos que se defina lo
+      contrario en esta Política de Privacidad, los términos utilizados en esta
+      Política de Privacidad tienen los mismos significados que en nuestros
+      Términos y Condiciones, accesibles desde Meethustle.io. Recopilación y uso
+      de información Recopilamos varios tipos diferentes de información con
+      diversas finalidades para proporcionar y mejorar nuestro Servicio. Tipos
+      de datos recopilados Datos personales Mientras utiliza nuestro Servicio,
+      es posible que le solicitemos que nos proporcione cierta información
+      personalmente identificable que puede incluir, entre otros, su nombre,
+      dirección de correo electrónico y número de teléfono. Los datos personales
+      (`Datos``) pueden ser utilizados para contactarlo o identificarlo. Datos
+      de uso También podemos recopilar información sobre cómo se accede y
+      utiliza el Servicio (`Datos de uso``). Estos Datos de uso pueden incluir
+      información como la dirección del protocolo de Internet de su computadora
+      (por ejemplo, dirección IP), tipo de navegador, versión del navegador, las
+      páginas de nuestro Servicio que visita, la hora y fecha de su visita, el
+      tiempo dedicado a esas páginas, identificadores de dispositivos y otros
+      datos de diagnóstico. Uso de datos Meethustle.io utiliza los datos
+      recopilados para diversos fines: Para proporcionar y mantener el Servicio
+      Para notificarle sobre cambios en nuestro Servicio Para permitirle
+      participar en funciones interactivas de nuestro Servicio cuando decida
+      hacerlo Para brindar asistencia al cliente Para recopilar análisis o
+      información valiosa que nos permitan mejorar nuestro Servicio Para
+      controlar el uso de nuestro Servicio Para detectar, prevenir y abordar
+      problemas técnicos Transferencia de datos Su información, incluidos los
+      Datos personales, puede ser transferida a —y mantenida en— computadoras
+      ubicadas fuera de su estado, provincia, país u otra jurisdicción
+      gubernamental donde las leyes de protección de datos pueden diferir de las
+      de su jurisdicción. Si usted se encuentra fuera de Argentina y elige
+      proporcionarnos información, tenga en cuenta que transferimos los datos,
+      incluidos los Datos personales, a Argentina y que los procesamos allí. Su
+      consentimiento a esta Política de Privacidad seguido de su envío de dicha
+      información representa su acuerdo con dicha transferencia. Meethustle.io
+      tomará todas las medidas razonablemente necesarias para garantizar que sus
+      datos sean tratados de forma segura y de conformidad con esta Política de
+      Privacidad y no se realizará ninguna transferencia de sus Datos personales
+      a una organización o país a menos que existan controles adecuados,
+      incluida la seguridad de sus datos y otra información personal.
+      Divulgación de datos Requisitos legales Meethustle.io puede divulgar sus
+      Datos personales de buena fe cuando considere que esta acción es necesaria
+      para: Cumplir con una obligación legal Proteger y defender los derechos o
+      la propiedad de Meethustle.io Prevenir o investigar posibles
+      irregularidades en relación con el Servicio Proteger la seguridad personal
+      de los usuarios del Servicio o del público Protegerse contra la
+      responsabilidad legal Seguridad de datos La seguridad de sus datos es
+      importante para nosotros, pero recuerde que ningún método de transmisión
+      por Internet o método de almacenamiento electrónico es 100% seguro. Si
+      bien nos esforzamos por utilizar medios comercialmente aceptables para
+      proteger sus Datos personales, no podemos garantizar su seguridad
+      absoluta. Proveedores de servicios Podemos emplear a empresas e individuos
+      de terceros para facilitar nuestro Servicio (`Proveedores de servicios``),
+      para proporcionar el Servicio en nuestro nombre, para realizar servicios
+      relacionados con el Servicio o para ayudarnos a analizar cómo se utiliza
+      nuestro Servicio. Estos terceros tienen acceso a sus Datos personales solo
+      para realizar estas tareas en nuestro nombre y están obligados a no
+      divulgarlos ni utilizarlos para ningún otro propósito. Enlaces a otros
+      sitios Nuestro Servicio puede contener enlaces a otros sitios que no son
+      operados por nosotros. Si hace clic en un enlace de un tercero, será
+      dirigido al sitio de ese tercero. Le recomendamos encarecidamente que
+      revise la Política de privacidad de cada sitio que visite. No tenemos
+      control ni asumimos responsabilidad alguna por el contenido, las políticas
+      de privacidad o las prácticas de sitios o servicios de terceros.
+      Privacidad de los menores Nuestro Servicio no está dirigido a personas
+      menores de 18 años (`Menores`). No recopilamos a sabiendas información
+      personalmente identificable de menores de 18 años. Si usted es padre/madre
+      o tutor y sabe que su hijo nos ha proporcionado Datos personales,
+      comuníquese con nosotros. Si nos enteramos de que hemos recopilado Datos
+      personales de menores sin verificación del consentimiento parental,
+      tomamos medidas para eliminar esa información de nuestros servidores.
+      Cambios en esta Política de Privacidad Podemos actualizar nuestra Política
+      de privacidad de vez en cuando. Le notificaremos cualquier cambio
+      publicando la nueva Política de privacidad en esta página. Le recomendamos
+      que revise esta Política de privacidad periódicamente para ver si hay
+      cambios. Los cambios a esta Política de privacidad son efectivos cuando se
+      publican en esta página. Contacte con nosotros Si tiene alguna pregunta
+      sobre esta Política de privacidad, puede ponerse en contacto con nosotros:
+      Por correo electrónico: joseandrescolmenares02@gmail.com
+    </div>
+  );
 };
+
+export default Privacy;
