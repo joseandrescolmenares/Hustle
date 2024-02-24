3c2c771c1d10729cbe3f0ae9bbf96f113ec2cb82
jose <joseandrescolmenares02@gmail.com>
2024-02-24 00:23:51 -0300

    fix: add tds

---
+++ src/app/tds/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const page = () => {
+  return (
+    <div>
+      <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Política de Tratamiento de Datos Personales{" "}
+      </h1>
+      <p className="w-2/5 mt-3 leading-10 [&:not(:first-child)]:mt-6">
+        1. Responsable del Tratamiento: Meethustle.io es responsable del
+        tratamiento de los datos personales de sus usuarios y clientes. 2. Datos
+        Personales Recopilados: Los datos personales que recopilamos incluyen
+        nombre, dirección de correo electrónico, número de teléfono, información
+        de pago y cualquier otra información que los usuarios decidan
+        proporcionar. 3. Finalidad del Tratamiento: Utilizamos los datos
+        personales recopilados para proporcionar nuestros servicios, mejorar su
+        experiencia como usuario, procesar pagos, enviar comunicaciones
+        relacionadas con el servicio y personalizar nuestro contenido y
+        publicidad. 4. Base Legal para el Tratamiento: La base legal para el
+        tratamiento de datos personales incluye el consentimiento del usuario,
+        la necesidad de cumplir con nuestras obligaciones contractuales, el
+        cumplimiento de obligaciones legales y el interés legítimo de
+        Meethustle.io en mejorar sus servicios y productos. 5. Seguridad de los
+        Datos: Implementamos medidas de seguridad técnicas, físicas y
+        administrativas para proteger los datos personales de accesos no
+        autorizados, pérdidas o alteraciones. 6. Transferencia de Datos: Podemos
+        transferir sus datos personales a terceros proveedores de servicios que
+        nos ayudan a proporcionar nuestros servicios, siempre garantizando la
+        seguridad y confidencialidad de los datos. 7. Derechos de los Titulares
+        de los Datos: Los titulares de los datos tienen derecho a acceder,
+        rectificar, suprimir, limitar u oponerse al tratamiento de sus datos
+        personales. Pueden ejercer estos derechos enviando una solicitud a
+        nuestro equipo de soporte. 8. Conservación de los Datos: Conservamos los
+        datos personales mientras sean necesarios para cumplir con los fines
+        para los que fueron recopilados, así como para cumplir con nuestras
+        obligaciones legales y resolver disputas. 9. Cambios en la Política de
+        Privacidad: Nos reservamos el derecho de modificar esta política en
+        cualquier momento. Se notificarán los cambios a los usuarios y se les
+        dará la oportunidad de revisar y aceptar los cambios antes de que entren
+        en vigencia. 10. Contacto: Si tiene alguna pregunta sobre nuestra
+        política de tratamiento de datos personales, puede ponerse en contacto
+        con nuestro equipo de soporte en max@meethustle.io
+      </p>
+    </div>
+  );
+};
