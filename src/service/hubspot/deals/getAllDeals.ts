a758f140875a7fe28edf68497537421bdd717a59
jose <joseandrescolmenares02@gmail.com>
2024-03-12 21:31:22 -0300

    fix: change

--- src/service/hubspot/deals/getAllDeals.ts
+++ src/service/hubspot/deals/getAllDeals.ts
@@ -24,30 +24,3 @@ export const getAllDeals = async (url: string) => {
   }
 };
 
-
-// export const getAllDeals = async (url: string) => {
-//   const cookiesStore = cookies();
-//   const cookieToken = cookiesStore.get("accessTokenHubspot")?.value;
-//   try {
-//     const headers = {
-//       Authorization: `Bearer ${cookieToken}`,
-//       "Content-Type": "application/json",
-//     };
-
-//     const urlDeals = url;
-
-//     const response = await fetch(urlDeals, { headers });
-    
-//     if (!response.ok) {
-//       throw new Error(`Error: ${response.status}`);
-//     }
-
-//     const responseData = await response.json();
-//     const dataCompanies = responseData;
-
-//     return dataCompanies;
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error();
-//   }
-// };
