4fbed5f051f7ccee218c8399cf1f4146e1489ccb
Jose andres Peña colmenares <joseandrespenacolmenares@192.168.1.16>
2024-05-27 20:22:19 -0300

    fix: refactor reply

--- src/service/hubspot/company/getAllCompanies.ts
+++ src/service/hubspot/company/getAllCompanies.ts
@@ -6,7 +6,7 @@ export const getAllCompanies = async () => {
   const cookieToken = cookiesStore.get("accessTokenHubspot")?.value;
   try {
     const headers = {
-      Authorization: `Bearer CKG0t9vSMRIUAAEDUAAA-SIAAED8BwkA4AcAAAQY792eFSD3hJkdKMXiigEyFGQUUPiocFajF7qROAEJfOpGUC5IOj0AAABBAAAAAMD_AwAAAAAAAIYAAAAAAAAADAAggI8APgDgMQAAAAAEwP__HwAQ8QMAAID__wMAAAAAAOABQhSwLT1IwIx9aYnXSsj5c5Vm0qfj5UoDbmExUgBaAA`,
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     };
     const getAllCompanies = "https://api.hubapi.com/crm/v3/objects/companies/";
--- src/service/whatsapp/reply/reply.ts
+++ src/service/whatsapp/reply/reply.ts
@@ -8,66 +8,60 @@ import { supabase } from "@/lib/ClientSupabase";
 
 export async function reply(dataMessage: any) {
   const cookieStore = cookies();
-  const supabaseClient = createRouteHandlerClient({
-    cookies: () => cookieStore,
-  });
+  const supabaseClient = createRouteHandlerClient({ cookies: () => cookieStore });
 
-  if ("statuses" in dataMessage.entry[0]?.changes[0]?.value) {
+  const messageEntry = dataMessage?.entry[0]?.changes[0]?.value;
+  if ("statuses" in messageEntry) {
     return;
   }
 
-  const phoneNumber = dataMessage.entry[0].changes[0].value.messages[0].from;
-  const messageTextStart =
-    dataMessage?.entry[0]?.changes[0]?.value.messages[0]?.text?.body;
-  const textCode =
-    dataMessage.entry[0]?.changes[0]?.value?.messages[0]?.type == "text";
+  const phoneNumber = messageEntry.messages[0].from;
+  const messageTextStart = messageEntry.messages[0]?.text?.body;
+  const isTextMessage = messageEntry.messages[0]?.type === "text";
 
-    if(textCode){ 
+  if (isTextMessage) {
+    const parts = messageTextStart?.split(" ");
+    if (parts.length > 0 && parts[0] === "start/") {
+      const afterStart = parts.slice(1).join(" ");
+      const validateCode = async () => {
+        const { data: dataUser, error } = await supabase
+          .from("users")
+          .update({ phoneNumber: phoneNumber })
+          .eq("codeTeam", afterStart)
+          .select();
+        
+        const user = dataUser?.[0]?.id_user;
 
-  const parts = messageTextStart?.split(" ");
-  if (parts.length > 0 && parts[0] === "start/") {
-    const afterStart = parts.slice(1).join(" ");
+        if (!user) {
+          sendMessage({
+            phoneNumber,
+            typeMessage: "text",
+            messageResponse: "Hubo un error con su código",
+          });
+        } else {
+          sendMessage({
+            phoneNumber,
+            typeMessage: "text",
+            messageResponse: `Bienvenido a Hustle 🎊🎊
 
-    const validatecode = async () => {
-      const { data: dataUser, error } = await supabase
-        .from("users")
-        .update({ phoneNumber: phoneNumber })
-        .eq("codeTeam", afterStart)
-        .select();
-      const user = dataUser?.[0].id_user;
+Desde ahora, puedes olvidarte de la operación (y el tiempo) que conlleva el CRM.
+Puedes hacer acciones como:
+➕ Crear un nuevo contacto, Max Velasco
+🔄 Asociar el contacto al negocio Hustle
+📞 Registrar una llamada a Jose Colmenares “Buscar en dos días”
+📅 Registrar una reunión con el negocio Hustle “Agregar minuta”
+⏰ Agregar una tarea al negocio Hustle para enviar la propuesta antes de mañana a las 2 PM
+📑 Hacer cargas/cambios masivos (Crear 10 empresas)
 
-      if (!user) {
-        sendMessage({
-          phoneNumber,
-          typeMessage: "text",
-          messageResponse: "hubo un error con su codigo",
-        });
-        return;
-      } else {
-        return sendMessage({
-          phoneNumber,
-          typeMessage: "text",
-          messageResponse: `Bienvenido a Hustle 🎊🎊
-  
-  Desde ahora, puedes olvidarte de la operación (y el tiempo) que conlleva el CRM 
-  Puedes hacer acciones como: 
-  ➕ Crear un nuevo contacto,Max Velasco
-  🔄 Asociar el contacto al negocio Hustle 
-  📞 Registrar una llamada a Jose Colmenares “Buscar en dos días”
-  📅 Registrar una reunión con el negocio Hustle “Aregar minuta”
-  ⏰ Agregar una tarea al negocio Hustle para enviar la propuesta antes de mañana a las 2 PM 
-  📑 Hacer cargas/cambios masivos (Crear 10 empresas)
-  
-  Ya sea con una nota de voz o en texto, 
-  se reflejará en tu CRM en segundos ⏱️
-            
-  ¡Disfruta tu nuevo copiloto! 🫂`,
-        });
-      }
-    };
-    return validatecode();
+Ya sea con una nota de voz o en texto, se reflejará en tu CRM en segundos ⏱️
+
+¡Disfruta tu nuevo copiloto! 🫂`,
+          });
+        }
+      };
+      return validateCode();
+    }
   }
-}
 
   if (!(await validateNumber(phoneNumber))?.validate?.status) {
     const message = await validateNumber(phoneNumber);
@@ -78,6 +72,7 @@ export async function reply(dataMessage: any) {
     };
     return sendMessage(response);
   }
+
   const { data: dataEmail, error } = await supabaseClient
     .from("users")
     .select("emailCrm")
@@ -87,7 +82,7 @@ export async function reply(dataMessage: any) {
 
   const email = dataEmail[0]?.emailCrm;
 
-  if (dataMessage.entry[0]?.changes[0]?.value?.messages[0]?.type == "audio") {
+  if (messageEntry.messages[0]?.type === "audio") {
     let messageResponseAudio = "¡Lo tengo! Procesando... 🎧🎧";
     const messageAudio = {
       messageResponseAudio,
@@ -96,51 +91,38 @@ export async function reply(dataMessage: any) {
     };
     sendMessage(messageAudio);
 
-    const id = dataMessage?.entry[0]?.changes[0]?.value?.messages[0].audio.id;
-
+    const id = messageEntry.messages[0]?.audio?.id;
     const transcribedText: string = await transcribeAudio(id);
 
-    messageResponseAudio = `🎧 Audio transcrito:
-  ${transcribedText}`;
-    const messageResponseTranscribed = {
+    messageResponseAudio = `🎧 Audio transcrito:\n${transcribedText}`;
+    sendMessage({
       messageResponseAudio,
       phoneNumber,
       typeMessage: "audio",
-    };
-    sendMessage(messageResponseTranscribed);
+    });
 
-    const responseBotWhatsappAudio = await agentAi(
-      transcribedText,
-      phoneNumber,
-      email
-    );
+    const responseBotWhatsappAudio = await agentAi(transcribedText, phoneNumber, email);
     messageResponseAudio = responseBotWhatsappAudio?.output;
-    console.log(transcribedText, "transcribedText");
-    const messageResponseAudioAgent = {
+    sendMessage({
       messageResponseAudio,
       phoneNumber,
       typeMessage: "audio",
-    };
-    sendMessage(messageResponseAudioAgent);
+    });
     return;
   }
-  // if (dataMessage.entry[0]?.changes[0]?.value?.messages[0]?.type == "audio")
-  //   return;
 
-  const messageBody =
-    dataMessage?.entry[0]?.changes[0]?.value.messages[0]?.text?.body;
-  let messageResponse = "¡Lo tengo! Procesando...";
-  const obj2 = { messageResponse, phoneNumber, typeMessage: "text" };
-  sendMessage(obj2);
+  const messageBody = messageEntry.messages[0]?.text?.body;
+  sendMessage({
+    messageResponse: "¡Lo tengo! Procesando...",
+    phoneNumber,
+    typeMessage: "text",
+  });
 
-  console.log(email, email);
   const responseBotWhatsapp = await agentAi(messageBody, phoneNumber, email);
-  messageResponse = responseBotWhatsapp?.output;
-
-  console.log("llegue hasta abajo en reply");
-
-  const response = { phoneNumber, messageResponse, typeMessage: "text" };
-  sendMessage(response);
-
-  return;
+  const messageResponse = responseBotWhatsapp?.output;
+  sendMessage({
+    messageResponse,
+    phoneNumber,
+    typeMessage: "text",
+  });
 }
